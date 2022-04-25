c0dea16e58c7f092c92d7bd44be7165fa4b3e932
Lukas Kaufmann <lukas.kaufm@outlook.com>
2022-04-25 20:19:44 +0200

    automatic build of front-end

---
+++ src/main/js/smart-house-control-panel/src/components/ControlPanel.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ControlPanel() {
+    return (
+        <div className="ControlPanel">
+            <p>Control panel, hello World</p>
+        </div>
+    );
+}
+
+export default ControlPanel;
---
+++ src/main/js/smart-house-control-panel/src/components/simulators/WeatherSimulator.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function WeatherSimulator() {
+
+}
+
+export default WeatherSimulator;
