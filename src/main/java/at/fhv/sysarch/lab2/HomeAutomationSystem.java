4089c0c79c689e2d9fee494dbf3ad26cc6d358b1
Lukas Kaufmann <lukas.kaufm@outlook.com>
2022-04-25 16:31:26 +0200

    integrated spring boot and react-typescript

--- src/main/java/at/fhv/sysarch/lab2/HomeAutomationSystem.java
+++ src/main/java/at/fhv/sysarch/lab2/HomeAutomationSystem.java
@@ -2,10 +2,14 @@ package at.fhv.sysarch.lab2;
 
 import akka.actor.typed.ActorSystem;
 import at.fhv.sysarch.lab2.homeautomation.HomeAutomationController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class HomeAutomationSystem {
 
     public static void main(String[] args) {
+        SpringApplication.run(HomeAutomationSystem.class);
         ActorSystem<Void> home = ActorSystem.create(HomeAutomationController.create(), "HomeAutomation");
     }
 
